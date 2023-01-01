ec608679786980c161e3acdee8b3682891627d34
Miguel Sanches Rocha <miguel.rocha@algartelecom.com.br>
2023-01-01 14:13:25 -0300

    Second Commit

--- grafos.py
+++ grafos.py
@@ -2,6 +2,14 @@ import igraph as ig
 import matplotlib.pyplot as plt
 import cairo
 
+def menu_geral():
+    print("Selecione uma etapa da Copa do Mundo FIFA 2022:")
+    print("1 - FASE DE GRUPOS")
+    print("2 - OITAVAS DE FINAL")
+    print("3 - QUARTAS DE FINAL")
+    print("4 - SEMI FINAL")
+    print("5 - FINAL")
+    print("0 - Encerrar Programa")
 
 def menu():
     print("Selecione um grupo da Copa do Mundo FIFA 2022:")
@@ -51,21 +59,42 @@ class grafo_fase_de_grupos():
 class grafo_oitavas_de_final():
     def __init__(self, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16):
 
-        self.g = ig.Graph([(0,1), (0,2), (0,3), (1,2), (1,3), (2,3)])
-        self.g.vs["name"] = [t1, t2, t3, t4]
-        self.g.vs["pass"] = ["t", "t", "f", "f", "f"]
+        self.g = ig.Graph([(0,1), (2,3), (4,5), (6,7), (8,9), (10,11), (12,13), (14,15)])
+        self.g.vs["name"] = [t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16]
+        self.g.vs["pass"] = ["t", "f", "t", "f", "f","t", "t", "f", "t", "f", "t","f", "t", "f", "t", "f"]
         layout = self.g.layout("kk")
         fig, ax = plt.subplots()
         self.g.vs["label"] = self.g.vs["name"]
         color_dict = {"t": "green", "f": "red"}
         self.g.vs["color"] = [color_dict[value] for value in self.g.vs["pass"]]
 
-        self.g.es[0]["color"] = "orange"
-        self.g.es[1]["color"] = "purple"
-        self.g.es[2]["color"] = "blue"
-        self.g.es[3]["color"] = "blue"
-        self.g.es[4]["color"] = "purple"
-        self.g.es[5]["color"] = "orange"
+        self.g.es["color"] = "black"
+
+
+        ig.plot(self.g, layout=layout, bbox=(300, 300), margin=20)
+        ig.plot(self.g, layout=layout, bbox=(300, 300), margin=20, target=ax) 
+
+        ig.plot(self.g, layout=layout)
+
+        string = "COPA DO MUNDO FIFA 2022 - OITAVAS DE FINAL"
+
+        plt.title(string)
+        plt.show()
+    
+class grafo_quartas_de_final():
+    def __init__(self, t1, t2, t3, t4, t5, t6, t7, t8):
+
+        self.g = ig.Graph([(0,1), (2,3), (4,5), (6,7)])
+        self.g.vs["name"] = [t1, t2, t3, t4, t5, t6, t7, t8]
+        self.g.vs["pass"] = ["f", "t", "t", "f", "f","t", "t", "f", "t", "f", "t","f", "t", "f", "t", "f"]
+        layout = self.g.layout("kk")
+        fig, ax = plt.subplots()
+        self.g.vs["label"] = self.g.vs["name"]
+        color_dict = {"t": "green", "f": "red"}
+        self.g.vs["color"] = [color_dict[value] for value in self.g.vs["pass"]]
+
+        self.g.es["color"] = "black"
+
 
         ig.plot(self.g, layout=layout, bbox=(300, 300), margin=20)
         ig.plot(self.g, layout=layout, bbox=(300, 300), margin=20, target=ax) 
@@ -78,8 +107,7 @@ class grafo_oitavas_de_final():
         plt.show()
 
 
-def main():
-
+def fase_de_grupos():
     menu()
     opcao = -1
     while opcao != 0:
@@ -104,6 +132,24 @@ def main():
             case 0:
                 break
 
+def oitavas_de_final():
+    grafo_oitavas_de_final("HOLANDA", "EUA", "ARGENTINA", "AUSTRÁLIA", "JAPÃO", "CROÁCIA", "BRASIL", "COREIA DO SUL", "INGLATERRA", "SENEGAL", "FRANÇA", "POLÔNIA", "MARROCOS", "ESPANHA", "PORTUGAL", "SUIÇA")
+
+def quartas_de_final():
+    grafo_quartas_de_final("HOLANDA", "ARGENTINA", "CROÁCIA", "BRASIL", "INGLATERRA", "FRANÇA", "MARROCOS", "PORTUGAL")
+
+
+def main():
+    menu_geral()
+    opcao_geral = -1
+    opcao_geral = int(input("\nOpção: "))
+    match opcao_geral:
+        case 1:
+            fase_de_grupos()
+        case 2:
+            oitavas_de_final()
+        case 3:
+            quartas_de_final()
 
 if __name__ == '__main__':
    main()
